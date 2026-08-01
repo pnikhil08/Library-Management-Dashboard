0e30f59074694fd25e48cc2d9b0f6d096eb55d3c
pnikhil08 <nikhilpandey5270@gmail.com>
2026-08-01 23:10:14 +0530

    fix code issue

--- middleware/auth.middileware.js
+++ middleware/auth.middileware.js
@@ -1,9 +1,8 @@
 
 const userValidation = (re, res, next)=>{
      console.log("User validate perfect")
-     console.log(`${re.method} and ${re.url}`)
 
+     console.log(`${re.method} and ${re.url}`)
      next()
 }
-
 module.exports = userValidation
