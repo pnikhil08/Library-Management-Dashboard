bd71ddd00cd54c1ade214ee9b9f88d0dfa0d36d7
Nikhil Pandey <116251187+pnikhil08@users.noreply.github.com>
2026-09-11 18:39:59 +0530

    Fix typos and improve error handling in Todo controller

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,23 +1,36 @@
-const Todo = require("../models/todo.model")
-
-exports.createTodo = async(re , res) =>{
-    try{const todo = await Todo.create(re.body);
-    res.status(201).json(todo);
-}catch(err){
-    res.status(500).json({
-        massage : err.massage
-    })
-}
-}
+// Import Todo model
+const Todo = require("../models/todo.model");
 
 
-exports.getAllTodo = async(re, res) => {
-    try{
-        const allTodo = await Todo.find();
-        res.status(200).json(allTodo)
-    }catch(err){
+// Create a new Todo
+// Receives Todo data from the request body and saves it to the database
+exports.createTodo = async (req, res) => {
+    try {
+        const todo = await Todo.create(req.body);
+
+        // Send the newly created Todo with 201 Created status
+        res.status(201).json(todo);
+    } catch (err) {
+        // Handle server/database errors
         res.status(500).json({
-            massage : err.massage
-        })
+            message: err.message
+        });
     }
-}
+};
+
+
+// Get all Todos
+// Fetches all Todo documents from the database
+exports.getAllTodo = async (req, res) => {
+    try {
+        const allTodo = await Todo.find();
+
+        // Send all Todos with 200 OK status
+        res.status(200).json(allTodo);
+    } catch (err) {
+        // Handle server/database errors
+        res.status(500).json({
+            message: err.message
+        });
+    }
+};
