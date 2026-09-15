7203066b20d0c7c1553ff75b85939ff5c2fee2f7
Nikhil Pandey <116251187+pnikhil08@users.noreply.github.com>
2026-09-15 23:20:16 +0530

    Add some features and Remove redundant comments in user controllers

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,24 +1,17 @@
-// Get all users
-// Sends a simple response containing all users
+
 exports.getUsers = (req, res) => {
     res.send("All User");
 };
-
-
 // Get user profile
 // Returns the user's profile information as a JSON response
 exports.getProfile = (req, res) => {
     res.json("This is user Profile");
 };
-
-
 // Register a new user
 // Sends a success message after user registration
 exports.userRegistration = (req, res) => {
     res.json("User Registration Successful");
 };
-
-
 // Login user
 // Returns basic user information along with a login success message
 exports.userLogin = (req, res) => {
